d51fedbcf4bce7083b48920cfc6814e85ca22ba6
JolyPhone <18990341730@163.com>
2022-02-13 21:22:19 +0800

    init

--- src/main/java/com/manning/readinglist/ReadinglistApplication.java
+++ src/main/java/com/manning/readinglist/ReadinglistApplication.java
@@ -2,7 +2,10 @@ package com.manning.readinglist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+* @author jrl
+* @date 2022/2/13
+*/
 @SpringBootApplication
 public class ReadinglistApplication {
 
